fcfca340800749efe06438ea890abf93e930bed8
Taiwo Ogunola <uniqueomokenny@gmail.com>
2019-05-02 22:20:18 +0100

    auth, ++auth() middleware, ++userSchema.statics.findByToken()

--- server/server.js
+++ server/server.js
@@ -16,9 +16,27 @@ app.use(cookieParser());
 // MODELS
 const { User } =  require('./models/user');
 
+// Middlewares
+const { auth } = require('./middleware/auth');
+
 // =======================================
 //                USERS
 // =======================================
+
+// Auth route
+app.get('/api/users/auth', auth, (req, res) => {
+  res.status(200).json({
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    cart: req.user.cart,
+    history: req.user.history,
+  })
+})
+
 // User registration
 app.post('/api/users/register', (req, res) => {
   const user = new User(req.body);
@@ -26,8 +44,7 @@ app.post('/api/users/register', (req, res) => {
   user.save((err, doc) => {
     if(err) return res.json({success: false, err});
     res.status(200).json({
-      success: true,
-      userdata: doc
+      success: true
     })
   })
 })
