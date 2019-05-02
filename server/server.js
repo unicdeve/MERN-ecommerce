e9092525e619a2dad87e6d001fc43f88e384e11e
Taiwo Ogunola <uniqueomokenny@gmail.com>
2019-05-02 18:59:27 +0100

    User model

--- server/server.js
+++ server/server.js
@@ -13,6 +13,15 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+// MODELS
+const { User } =  require('./models/user');
+
+// =======================================
+//                USERS
+// =======================================
+app.post('api/users/register', (req, res) => {
+  res.status(200);
+})
 
 const port = process.env.PORT || 3002;
 
