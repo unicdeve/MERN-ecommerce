0b15451988b8617815234b2a2cee392bdab0cc0a
Taiwo Ogunola <uniqueomokenny@gmail.com>
2019-05-02 21:30:03 +0100

    user login and token generation: ++userSchema.methods.comparePassword() & generateToken()

--- server/server.js
+++ server/server.js
@@ -19,6 +19,7 @@ const { User } =  require('./models/user');
 // =======================================
 //                USERS
 // =======================================
+// User registration
 app.post('/api/users/register', (req, res) => {
   const user = new User(req.body);
   
@@ -27,6 +28,24 @@ app.post('/api/users/register', (req, res) => {
     res.status(200).json({
       success: true,
       userdata: doc
+    })
+  })
+})
+
+// Login
+app.post('/api/users/login', (req, res) => {
+  User.findOne({'email': req.body.email}, (err, user) => {
+    if(!user) return res.json({loginSuccess: false, message: "Auth failed, email not found"});
+
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if(!isMatch) return res.json({loginSuccess: false, message: "Wrong password"});
+
+      user.generateToken((err, user) => {
+        if(err) return res.status(400).send(err);
+        res.cookie('w_auth', user.token).status(200).json({
+          loginSuccess: true
+        })
+      })
     })
   })
 })
