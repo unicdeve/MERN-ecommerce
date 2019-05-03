5d1a2fa3146baec1cc2960d264dde8b87fc466da
Taiwo Ogunola <uniqueomokenny@gmail.com>
2019-05-03 23:03:17 +0100

    Header and Footer components

---
+++ client/src/components/Header_footer/Header/index.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+export default class Header extends Component {
+  render() {
+    return (
+      <header className="bck_b_light">
+        <div className="container">
+          <div className="left">
+          <div className="logo">
+            WAVES
+          </div>
+          </div>
+          <div className="right">
+            <div className="top">
+              LINK
+            </div>
+            <div className="bottom">
+              LIMKS
+            </div>
+          </div>
+        </div>
+      </header>
+    )
+  }
+}
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { BrowserRouter } from 'react-router-dom'
+import Routes from './routes';
+import './Resources/css/styles.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes />
+  </BrowserRouter>,
+document.getElementById('root'));
