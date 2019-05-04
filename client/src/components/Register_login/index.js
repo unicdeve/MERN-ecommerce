b8ead132b131212a499dc0582d6d8e7c39ef5407
Taiwo Ogunola <uniqueomokenny@gmail.com>
2019-05-04 01:38:07 +0100

    FormField component with formActions :  ++validate(), ++update()

--- client/src/components/Register_login/index.js
+++ client/src/components/Register_login/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import MyButton from '../utils/button';
 
+import Login from './login';
+
 const RegisterLogin = () => {
   return (
     <div className="page_wrapper">
@@ -22,7 +24,7 @@ const RegisterLogin = () => {
           <div className="right">
               <h2>Registered customers</h2>
               <p>If you have an account, please log in</p>
-              LOGIN
+              <Login />
           </div>
         </div>
       </div>
---
+++ client/src/components/Register_login/login.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import FormField from '../utils/Form/formfield';
+
+import { update } from '../utils/Form/formActions';
+
+class Login extends Component {
+
+  state = {
+    formError: false,
+    formSuccess: '',
+    formdata: {
+      email: {
+        element: 'input',
+        value: '',
+        config: {
+          name: "email_inpout",
+          type: "email",
+          placeholder: "Enter your email"
+        },
+        validation: {
+          required: true,
+          // email: true
+        },
+        valid: false,
+        touched: false,
+        validationMessage: ""
+      },
+      password: {
+        element: 'input',
+        value: '',
+        config: {
+          name: "password_inpout",
+          type: "password",
+          placeholder: "Enter your password"
+        },
+        validation: {
+          required: true
+        },
+        valid: false,
+        touched: false,
+        validationMessage: ""
+      }
+    }
+  }
+  
+  updateForm = (element) => {
+    const newFormdata = update(element, this.state.formdata, "login");
+    this.setState({
+      formError: false,
+      formdata: newFormdata
+    })
+  }
+
+  submitForm = () => {
+
+  }
+
+  render() {
+    return (
+      <div className="signin_wrapper">
+        <form onSubmit={(event) => this.submitForm(event)}>
+          <FormField 
+            id={'email'}
+            formdata={this.state.formdata.email}
+            change={(element) => this.updateForm(element)}
+          />
+
+          <FormField 
+            id={'password'}
+            formdata={this.state.formdata.password}
+            change={(element) => this.updateForm(element)}
+          />
+        </form>
+      </div>
+    )
+  }
+}
+
+export default connect()(Login)
