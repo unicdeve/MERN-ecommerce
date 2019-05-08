596ed7f72b39a7b6cd3d6aeac22084c17c9987b3
Taiwo Ogunola <uniqueomokenny@gmail.com>
2019-05-08 18:25:17 +0100

    product actions: getProductsBySell() and getProductsByArrival() - for home product datas

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import HomeSlider from './home_slider';
 import HomePromotion from './home_promotion';
 
-const Home = () => {
-  return (
-    <div>
-      <HomeSlider />
-      <HomePromotion />
-    </div>
-  );
+import { connect } from 'react-redux';
+import { getProductsByArrival, getProductsBySell } from '../../actions/product_action';
+
+class Home extends Component {
+
+  componentDidMount() {
+    this.props.dispatch(getProductsBySell());
+    this.props.dispatch(getProductsByArrival());
+  }
+
+  render() {
+    return (
+      <div>
+        <HomeSlider />
+        <HomePromotion />
+      </div>
+    );
+  }
 };
 
-export default Home;
+const mapStateToProps = (state) => {
+  return {
+    products: state.products
+  }
+}
+
+export default connect(mapStateToProps)(Home);
