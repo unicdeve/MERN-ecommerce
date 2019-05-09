7b38e6c80c4b0059b1c7740cd05723e409733cb4
Taiwo Ogunola <uniqueomokenny@gmail.com>
2019-05-09 13:39:41 +0100

    card blocks for getProductsBySell and getProductsByArrival: CardBlock and Card components for displaying the products

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import HomeSlider from './home_slider';
 import HomePromotion from './home_promotion';
+import CardBlock from '../utils/card_block';
 
 import { connect } from 'react-redux';
 import { getProductsByArrival, getProductsBySell } from '../../actions/product_action';
@@ -16,7 +17,15 @@ class Home extends Component {
     return (
       <div>
         <HomeSlider />
+        <CardBlock 
+          list={this.props.products.bySell}
+          title="Best Selling Guitars"
+        />
         <HomePromotion />
+        <CardBlock 
+          list={this.props.products.byArrival}
+          title="new arrivals"
+        />
       </div>
     );
   }
--- client/src/components/utils/button.js
+++ client/src/components/utils/button.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import FontAwesonIcon from '@fortawesome/react-fontawesome';
+import faShoppingBag from '@fortawesome/fontawesome-free-solid/faShoppingBag';
 
 const MyButton = (props) => {
 
@@ -10,13 +12,25 @@ const MyButton = (props) => {
       case "default":
         template = (
           <Link
-            className="link_default"
+            className={!props.altClass ? "link_default" : props.altClass}
             to={props.linkTo}
             {...props.addStyles}
           >
             {props.title}
           </Link>
         )
+      break;
+
+      case "bag_link":
+          template = (
+            <div className="bag_link"
+              onClick={() => { props.runAction() }}
+            >
+              <FontAwesonIcon 
+                icon={faShoppingBag}
+              />
+            </div>
+          )
       break;
     }
     return template;
