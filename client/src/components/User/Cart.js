9bcdbf9e5756d627a825d30bba00b2d6a3f7768e
Taiwo Ogunola <uniqueomokenny@gmail.com>
2019-05-14 17:45:58 +0100

    MyCart: ++calculateTotal()

--- client/src/components/User/Cart.js
+++ client/src/components/User/Cart.js
@@ -30,15 +30,36 @@ class UserCart extends Component {
 
         this.props.dispatch(getCartItems(cartItems, user.userData.cart))
         .then(() => {
-          
+          if(this.props.user.cartDetail.length > 0) {
+            this.calculateTotal(this.props.user.cartDetail);
+          }
         })
       }
     }
   }
 
+  calculateTotal = (cartDetail) => {
+    let total = 0;
+    cartDetail.forEach(item => {
+      total += parseInt(item.price, 10) * item.quantity;
+    });
+
+    this.setState({
+      total,
+      showTotal: true
+    })
+  }
+
   removeFromCart = () => {
 
   }
+
+  showNoItemMessage = () => (
+    <div className="cart_no_items">
+      <FontAwesomeIcon icon={faFrown} />
+      <div>You have no items</div>
+    </div>
+  )
 
   render() {
     return (
@@ -51,7 +72,30 @@ class UserCart extends Component {
               type="cart"
               reomveItem={(id) => this.removeFromCart(id)}
             />
+            {
+              this.state.showTotal ?
+                <div className="user_cart_sum">
+                  <div>Total amount: $ {this.state.total}</div>
+                </div>
+                :
+                this.state.showSuccess ?
+                  <div className="cart_success">
+                    <FontAwesomeIcon icon={faSmile} />
+                    <div>THANK YOU</div>
+                    <div>YOUR ORDER IS NOW COMPLETE</div>
+                  </div>
+                  :
+                  this.showNoItemMessage()
+            }
           </div>
+          {
+            this.state.showTotal ?
+              <div className="paypal_button_container">
+                Paypal
+              </div>
+              :
+              null
+          }
         </div>
       </UserLayout>
     )
